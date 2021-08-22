448990f2ab0b1d6f34d151004c582e0fa5755f39
YiGeon <parkegun@gmail.com>
2021-08-22 23:59:12 +0900

    영와 정보 그리드 적용

--- src/components/MovieDetailInfo.js
+++ src/components/MovieDetailInfo.js
@@ -1,36 +1,51 @@
 import React from 'react';
-import {makeStyles, Paper, Typography} from "@material-ui/core";
+import {Grid, makeStyles, Paper, Typography} from "@material-ui/core";
 
 const MovieDetailInfo = ({movieData}) => {
     const useStyles = makeStyles((theme) => ({
-        poster: {
-            width: '300px'
-        }
-
-    }))
+        root: {
+            width: '80%',
+            flexGrow: 1,
+        },
+        paper: {
+           width: '300px'
+        },
+    }));
     const classes = useStyles();
 
     return (
         <div>
-            <Typography variant="h6">
-                영화제목 : {movieData.movienm}
-            </Typography>
-            <Typography variant="h6">
-                개봉일 : {movieData.opendt}
-            </Typography>
+            <Grid container spacing={3} justifyContent="center" direction="row-reverse">
 
-            <Paper className={classes.poster} variant="outlined" elevation={3}>
-                <img style={{width: '100%'}} src={movieData.posterurl} alt={'영화포스터'}/>
-            </Paper>
-            <Typography variant="h6">
-                상태 : {movieData.prdtstatnm}
-            </Typography>
-            <Typography variant="h6">
-                제작년도 : {movieData.prdtyear}
-            </Typography>
-            <Typography variant="h6">
-                제작국가 : {movieData.repnationnm}
-            </Typography>
+                <Grid item xs={8}>
+                    <Paper className={classes.paper}>
+                        <img style={{width: '100%'}} src={movieData.posterurl}
+                             alt={'영화포스터'}/>
+                    </Paper>
+                </Grid>
+
+
+                <Grid item xs={4}>
+                    <Typography variant="h6">
+                        영화제목 : {movieData.movienm}
+                    </Typography>
+                    <Typography variant="h6">
+                        개봉일 : {movieData.opendt}
+                    </Typography>
+                    <Typography variant="h6">
+                        상태 : {movieData.prdtstatnm}
+                    </Typography>
+                    <Typography variant="h6">
+                        제작년도 : {movieData.prdtyear}
+                    </Typography>
+                    <Typography variant="h6">
+                        제작국가 : {movieData.repnationnm}
+                    </Typography>
+                </Grid>
+
+            </Grid>
+
+
 
         </div>
     );
