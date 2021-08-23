dd411211c2859806901e980b69301c656303f162
YiGeon <parkegun@gmail.com>
2021-08-23 22:51:10 +0900

    메뉴바 레이아웃 조정

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -2,25 +2,31 @@ import React from 'react';
 import {makeStyles, MenuItem, MenuList, Paper, Typography} from "@material-ui/core";
 
 const Menubar = () => {
-    const useStyles = makeStyles({
+    const useStyles = makeStyles((theme) => ({
         root: {
+            marginLeft: 160,
             position: 'absolute',
             height: '100%',
-            background: '#F3F3F3'
+            background: '#F3F3F3',
         },
-    });
+        menuItem: {
+            marginTop: 50,
+            color: theme.palette.secondary.main,
+            fontWeight: 'bold',
+        }
+    }));
 
     const classes = useStyles();
     return (
         <Paper className={classes.root}>
             <MenuList>
-                <MenuItem>
+                <MenuItem className={classes.menuItem}>
                     <Typography variant="inherit">GENRE</Typography>
                 </MenuItem>
-                <MenuItem>
+                <MenuItem className={classes.menuItem}>
                     <Typography variant="inherit">MOVIE RANKING</Typography>
                 </MenuItem>
-                <MenuItem>
+                <MenuItem className={classes.menuItem}>
                     <Typography variant="inherit">REVIEW</Typography>
                 </MenuItem>
             </MenuList>
