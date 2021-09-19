792466cdd2bad54f949daa2ef1a080740dd9412a
YiGeon <parkegun@gmail.com>
2021-09-19 21:48:31 +0900

    푸터 css 수정

--- src/components/GridContainer.js
+++ src/components/GridContainer.js
@@ -2,6 +2,6 @@ import styled from "styled-components";
 import {Grid} from "@material-ui/core";
 
 export const GridContainer = styled(Grid)`
-  height: max(100vh, 100%);
+  min-height: calc(100vh - (61px + 40px));
   
 `
