4a0e1fbe2ca0b55aa0ab9e584449f72629c4c4b2
YiGeon <parkegun@gmail.com>
2021-09-22 02:45:30 +0900

    검색 결과 무한 스크롤 페이징

--- src/components/MovieCardList.js
+++ src/components/MovieCardList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Card, CardContent, Grid, Typography} from "@material-ui/core";
 import {useHistory} from "react-router-dom";
 
-const MovieCardList = ({backgroundColor, movieList}) => {
+const MovieCardList = ({backgroundColor, movieList, inViewRef}) => {
     console.log(movieList)
     const onErrorImg = require('../assets/defaultPoster.png').default;
     const history = useHistory();
@@ -11,20 +11,22 @@ const MovieCardList = ({backgroundColor, movieList}) => {
     return (
         <Grid container spacing={3} style={{backgroundColor: backgroundColor, marginTop: '20px'}}>
             {movieList?.map((movie, i) => (
-                <Grid item xs={3}>
-                    <Card item key={i} style={{
+                <Grid item xs={3} ref={movieList.length - 1 === i ? (inViewRef ?? undefined) : undefined} key={i}>
+                    <Card item style={{
                         display: 'flex',
                         alignItems: 'center',
                         justifyContent: 'center',
                         backgroundColor: '#E0E0E0',
                         textAlign: 'center',
                         height: '280px',
-                        cursor:'pointer',
+                        cursor: 'pointer',
                     }}
-                    onClick={() => history.push('/movie/' + movie.moviecd)}>
+                          onClick={() => history.push('/movie/' + movie.moviecd)}>
                         <CardContent>
                             <img style={{height: '230px', margin: '3px 0'}} src={movie.posterurl}
-                                onError={e => {e.target.src = onErrorImg}} alt={'영화 포스터'}/>
+                                 onError={e => {
+                                     e.target.src = onErrorImg
+                                 }} alt={'영화 포스터'}/>
                             <Typography>
                                 {movie.movienm}
                             </Typography>
--- src/pages/SearchResult/SearchResultContainer.js
+++ src/pages/SearchResult/SearchResultContainer.js
@@ -1,16 +1,55 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TopBar from "../../components/TopBar";
 import {Grid, Typography} from "@material-ui/core";
 import Menubar from "../../components/Menubar";
 import {GridContainer} from "../../components/GridContainer";
-import {useQuery} from "react-query";
+import {useInfiniteQuery, useQuery} from "react-query";
 import {findMovieListByName} from "../../apis/MovieApi";
 import MovieCardList from "../../components/MovieCardList";
+import {useInView} from "react-intersection-observer";
 
 const SearchResultContainer = (props) => {
-    const searchList = useQuery(['searchList',  props.match.params['keyword']],
-        () => findMovieListByName.api(1, 10, props.match.params['keyword']),
+    const {ref, inView} = useInView();
+    const searchList = useQuery(['searchList', props.match.params['keyword']],
+        () => findMovieListByName.api(1, 8, props.match.params['keyword']),
         {keepPreviousData: true})
+
+    useEffect(() => {
+        fetchNextResult().then((res) => {
+            console.log(res)
+            console.log('페이징 아이템')
+        }).catch(e => {
+            console.log(e)
+        });
+    }, [inView])
+
+    const {
+        data: useResultListQuery,
+        fetchNextPage: fetchNextResult,
+    } = useInfiniteQuery(
+        ['userReviewList', props.match.params['keyword']],
+        ({
+             pageParam = {
+                 pageNum: 1,
+                 pageSize: 8,
+             },
+         }) =>
+            findMovieListByName.api(pageParam.pageNum, pageParam.pageSize, props.match.params['keyword']),
+        {
+            keepPreviousData: true,
+            getNextPageParam: lastPage => {
+                console.log(lastPage)
+                if (lastPage?.data.next === null) {
+                    console.log('마지막 페이지 없음')
+                    return undefined;
+                }
+                return {
+                    pageNum: (lastPage?.config?.params.page + 1),
+                    pageSize: 8,
+                };
+            },
+        },
+    );
 
     console.log(searchList);
     return (
@@ -22,10 +61,15 @@ const SearchResultContainer = (props) => {
                 </Grid>
 
                 <Grid item xs={8} style={{marginBottom: '60px'}}>
-                    <Typography color={'secondary'} variant={'h5'}  component={'h5'} style={{marginTop: '20px', fontWeight: 700}}>
+                    <Typography color={'secondary'} variant={'h5'} component={'h5'}
+                                style={{marginTop: '20px', fontWeight: 700}}>
                         RESULTS
                     </Typography>
-                    <MovieCardList movieList={searchList.data?.data.results} />
+                    {useResultListQuery?.pages.map((group, i) => (
+                        <div key={i}>
+                            <MovieCardList movieList={group.data?.results} inViewRef={ref}/>
+                        </div>
+                    ))}
                 </Grid>
 
                 <Grid item xs={1}/>
